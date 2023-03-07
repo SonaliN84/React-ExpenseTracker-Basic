3ac83df59aa5407d1750c38651a96199969033bf
SonaliN84 <sonali.nilekar@gmail.com>
2023-03-07 15:19:28 +0530

    Multiple States( listen to onChange event on Form inputs)

---
+++ Multiple States/src/component/FormExpense/FormExpense.js
@@ -0,0 +1,65 @@
+import {useState} from 'react'
+import './FormExpense.css'
+function FormExpense() {
+   const [enteredTitle,setTitle]=useState('')
+   const [enteredAmount,setAmount]=useState('')
+   const [enteredDate,setDate]=useState('')
+
+    const titleHandler=(event)=>{
+        console.log(event.target.value)
+        setTitle(event.target.value)
+    }
+
+    const AmountHandler=(event)=>{
+        console.log(event.target.value)
+        setAmount(event.target.value)
+    }
+
+    const DateHandler=(event)=>{
+        console.log(event.target.value)
+        setDate(event.target.value)
+
+    }
+
+
+
+
+  return (
+    <div className="container my-4">
+    <h3 className='my-4'>Enter Expense details</h3>
+    <form>
+      <div className="form-group">
+        <label for="expenseTitle">Expense Title</label>
+        <input
+          type="text"
+          className="form-control"
+          id="expenseTitle"
+          onChange={titleHandler}
+        />
+      </div>
+      <div className="form-group my-3">
+        <label for="expenseAmount">Expense Amount</label>
+        <input
+          type="text"
+          className="form-control"
+          id="expenseAmount"
+          onChange={AmountHandler}
+        />
+      </div>
+      <div className="form-group my-3">
+        <label for="expenseDate">Expense Date</label>
+        <input
+          type="date"
+          className="form-control"
+          id="expenseDate"
+          onChange={DateHandler}
+        />
+      </div>
+      <div className="form-group my-3">
+        <button className='btn btn-primary btn-block my-4'>Add expense</button>
+      </div>
+    </form>
+    </div>
+  );
+}
+export default FormExpense;
