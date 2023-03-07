abbfb8d2ae5a7f937089f50233bba71f7e4dfc81
SonaliN84 <sonali.nilekar@gmail.com>
2023-03-07 11:20:18 +0530

    useState

---
+++ useState/src/component/Expense/ExpenseDetails.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './ExpenseDetails.css'
+function ExpenseDetails(props){
+const [Amount,setAmount]=useState(props.Amount)
+function clickHandler(){
+  setAmount("Rs.100")
+}
+
+  return (
+    <div className="details">
+  <div className="a">{props.title}</div>
+  <div className="a">{Amount}
+  <button className="buttont" onClick={clickHandler}>Change</button>
+  </div>
+  <div className="a">{props.Location}</div>
+  </div>
+  );
+
+
+
+}
+export default ExpenseDetails;
