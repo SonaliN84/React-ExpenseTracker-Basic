ff17a4e5d6040a5b35f6338439aedaf1e6fdc5c6
SonaliN84 <sonali.nilekar@gmail.com>
2023-03-08 18:23:36 +0530

    Final-Expense Tracker

---
+++ Final-Expense Tracker/src/App.js
@@ -0,0 +1,49 @@
+import {useState} from 'react'
+import Expense from "./component/Expense/Expense";
+
+// import "./App.css";
+import NewExpense from "./component/FormExpense/NewExpense";
+const DummyExpense = [
+  { id:1,
+    date: new Date(2021, 2, 22),
+    title: "Car Insurance",
+    amount: 1000
+    
+  },
+  { id:2,
+    date: new Date(2022, 7, 12),
+    title: "Birthday",
+    amount: 2000
+    
+  },
+  { id:3,
+    date: new Date(2023, 2, 17),
+    title: "Shopping",
+    amount: 700
+   
+  }
+];
+function App() {
+  
+ const[expenses,setExpenses]= useState(DummyExpense);
+  
+  const addExpenseHandler=(expense)=>{
+    console.log(expense)
+    // setExpenses([...expenses,expense])
+    setExpenses((prevExpenses)=>{
+      return [expense,...prevExpenses]
+    })
+    
+  }
+
+  return (
+    <div>
+    <div className="p-3 mb-2 bg-info text-white"><h1 className="text-center  ">Expense Tracker</h1></div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expense items={expenses}/>
+   
+    </div>
+  );
+}
+
+export default App;
