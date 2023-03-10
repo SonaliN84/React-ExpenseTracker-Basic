92326ecf2ad828e8839db12b9587965ce44a21c4
SonaliN84 <sonali.nilekar@gmail.com>
2023-03-10 16:31:12 +0530

    Project 2 with Fragment and Modal portal

---
+++ project 2 with Fragment and Modal portal/src/components/UsersList/UserList.js
@@ -0,0 +1,15 @@
+import "./Userlist.css";
+function UserList(props) {
+  return (
+    <div>
+      <ul className="container">
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years)
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default UserList;
